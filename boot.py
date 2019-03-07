42cc681296783c87bed6e831c2ab90fa296a8942
tboettjer <tboettjer@outlook.com>
2019-03-07 17:39:30 +0100

    Create secrets file
    Separate WLAN credentials

--- boot.py
+++ boot.py
@@ -1,12 +1,14 @@
 import machine
 from network import WLAN
+from secrets import ACCESS
 wlan = WLAN(mode=WLAN.STA)
+access = ACCESS()
 
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == 'myssid':
+    if net.ssid == access.ssid():
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, 'mykey'), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, access.pwd()), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print('WLAN connection succeeded!')
