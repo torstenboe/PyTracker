08e3acc52c5f97f7b024f86a5d9a4e9623a597e6
tboettjer <tboettjer@outlook.com>
2019-03-09 10:47:46 +0100

    Change boot.py
    Based on the example of PiAir/pycom-libraries

--- boot.py
+++ boot.py
@@ -1,15 +1,36 @@
+# boot.py -- run on boot-up
+import os
+import time
+import pycom
 import machine
+import secrets
 from network import WLAN
-from secrets import ACCESS
-wlan = WLAN(mode=WLAN.STA)
-access = ACCESS()
+from machine import UART
+
+uart = UART(0, 115200)
+os.dupterm(uart)
+
+uart1 = UART(1, 9600)
+
+pycom.heartbeat(False)
+pycom.rgbled(0x7f7f00) # yellow
+
+wlan = WLAN() # get current object, without changing the mode
+
+if machine.reset_cause() != machine.SOFT_RESET:
+    wlan.init(mode=WLAN.STA)
+    # configuration below MUST match your home router settings!!
+    wlan.ifconfig(config=(secrets.WIFI_IP, secrets.WIFI_SUBNET, secrets.WIFI_GATEWAY, secrets.WIFI_DNS1))
 
 nets = wlan.scan()
+
 for net in nets:
-    if net.ssid == access.ssid():
-        print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, access.pwd()), timeout=5000)
+    if net.ssid == secrets.WIFI_SSID:
+        if not wlan.isconnected():
+            wlan.connect(secrets.WIFI_SSID, auth=(WLAN.WPA2, secrets.WIFI_PASS), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
-        print('WLAN connection succeeded!')
+        pycom.rgbled(0x007f00) # green
+        time.sleep(3)
+        pycom.rgbled(0)
         break
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import ubinascii
+from network import Bluetooth
+bluetooth = Bluetooth()
 
+bluetooth.set_advertisement(name="LoPy", service_uuid=b'1234567890123456')
+bluetooth.advertise(True)
