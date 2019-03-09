c71e1e6c32216354be58049dee4c8773f48bb5f0
tboettjer <tboettjer@outlook.com>
2019-03-09 11:01:04 +0100

    Create secrets_template.py
    A template of the secrects.py file

---
+++ lib/secrets_template.py
@@ -0,0 +1,15 @@
+""" LoPy LoRaWAN Nodeconfiguration options """
+
+# These you need to replace!
+DEV_ADDR = '26012549'
+NWK_SWKEY = '970C26E4B9D3BC2FC84474A087E86187'
+APP_SWKEY = '77F0E7E673F789FF9016AACDAE203353'
+
+# Leave them empty or unchanged if you don't want the
+# node to connect to WiFi
+WIFI_SSID = 'WiFi@boettjer.de'
+WIFI_PASS = 'mr4d-omwj-o93c'
+WIFI_IP = '192.168.1.230' # fixed IP
+WIFI_SUBNET = '255.255.255.0'
+WIFI_GATEWAY = '192.168.1.1'
+WIFI_DNS1 = '192.168.1.57'
