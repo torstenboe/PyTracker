7910d49dd8c250525044bc410f4d672563a18cab
Torsten <tboettjer@outlook.com>
2019-03-11 22:20:19 +0100

    Adding LORA code
    Onboard LoPy to TTN

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import machine
 from machine import I2C
+from network import LoRa
+import socket
+import binascii
+import struct
 import pycom
 import sys
 import time
-# see https://github.com/inmcm/micropyGPS/blob/master/micropyGPS.py
+
+# Setup the LoRaWAN part
+# Initialize LoRa in LORAWAN mode.
+lora = LoRa(mode=LoRa.LORAWAN)
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify(secrets.DEV_ADDR.replace(' ','')))[0]
+nwk_swkey = binascii.unhexlify(secrets.NWK_SWKEY.replace(' ',''))
+app_swkey = binascii.unhexlify(secrets.APP_SWKEY.replace(' ',''))
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+# remove all the non-default channels
+for i in range(3, 16):
+    lora.remove_channel(i)
+# set the 3 default channels to the same frequency
+lora.add_channel(0, frequency=868100000, dr_min=0, dr_max=5)
+lora.add_channel(1, frequency=868100000, dr_min=0, dr_max=5)
+lora.add_channel(2, frequency=868100000, dr_min=0, dr_max=5)
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+# make the socket blocking
+s.setblocking(False)
+
+# for microGPS see https://github.com/inmcm/micropyGPS/blob/master/micropyGPS.py
 from micropyGPS import MicropyGPS
 pycom.heartbeat(True)
 print("Initializing...")
